552b302781ce4a95a709c59c2c2c9c8b7212a7a2
vxyagr <hello.angkin@gmail.com>
2022-08-01 09:23:16 +0700

    claim dividend

--- components/DashboardPage/Dashboard.tsx
+++ components/DashboardPage/Dashboard.tsx
@@ -52,14 +52,17 @@ const Hero: FunctionComponent<HeroProps> = (props) => {
     //if (window.web3) {
 
     var contractAbi = require("../../abis/StonkerNFTABI.json");
+    var dividendContractAbi = require("../../abis/stonkerDividendABI.json");
     //const contractAddres = "0x21Bf9D1BBc6FA5C39378fe3091B05EF51d24BcFF"; //rinkeby
 
     const contractAddres = "0x6BdD2353D12a78FEa7487829adaB30bf391ae336"; //mainnet
+    const dividendContractAddres = "0x882249918044aF91FDCA1B7DE0FFbde637E5F546"; //mainnet
     // console.log(accountsList[0])
 
     // const provider = new providers.JsonRpcBatchProvider("https://rinkeby.infura.io/v3/8051d992532d4f65b1cea01cb751d577");
 
     const contract = new ethers.Contract(contractAddres, contractAbi, signer);
+    const dividendContract = new ethers.Contract(dividendContractAddres, dividendContractAbi, signer);
     const [lokaCards, setLokaCards] = useState([]);
     const prices = [10, 120, 10, 10];
     const [dataFetched, setDataFethced] = useState(false);
@@ -73,14 +76,45 @@ const Hero: FunctionComponent<HeroProps> = (props) => {
     const [vampire, setVampire] = useState(0);
     const [android, setAndroid] = useState(0);
     const [ownerPercent, setOwnerPercent] = useState(0);
+    const [claimable, setClaimable] = useState(0);
+    const [totalClaimed, setTotalClaimed] = useState(0);
     const [zeroStonker, setZeroStonker] = useState(true);
-    const totalStonkerYield = 9822;
+    const [totalStonkerYield, setTotalStonkerYield] = useState(0);
 
     const [cardData, setCardData] = useState([]);
+
+    const getTotalYield = async () => {
+        if (account) {
+            var totalYield = await dividendContract.getPopulationYield();
+            setTotalStonkerYield(totalYield);
+        }
+    };
+
+    const getClaimableDividend = async () => {
+        if (account) {
+            var claimable = await dividendContract.getClaimableDividend(account);
+            setClaimable((claimable * 0.7) / 1000000);
+        }
+    };
+
+    const claim = async () => {
+        if (account) {
+            var claimable = await dividendContract.claim();
+            //setClaimable((claimable * 0.7) / 1000000);
+        }
+    };
+
+    const getTotalClaimed = async () => {
+        if (account) {
+            var totalClaim = await dividendContract.getTotalClaimedDividends(account);
+            setTotalClaimed(totalClaim);
+        }
+    };
 
     const getOwned = async () => {
         if (account) {
             var owned = await contract.stonkersOfOwner(account);
+            getTotalYield();
             //console.log(owned);
             for (const [index, value] of owned.entries()) {
                 var card_: ICard = { tokenId: 0, yield: 0 };
@@ -110,83 +144,12 @@ const Hero: FunctionComponent<HeroProps> = (props) => {
             var ownedPercent = ((1 * human + 1.3 * elf + 1.6 * vampire + 2 * android) / totalStonkerYield) * 100;
             setOwnerPercent(ownedPercent);
             if (ownedPercent > 0) setZeroStonker(false);
-            /*availableSpecies[0] = await contract.getAvailableSpecies(0);
-        availableSpecies[1] = await contract.getAvailableSpecies(1);
-        availableSpecies[2] = await contract.getAvailableSpecies(2);
-        availableSpecies[3] = await contract.getAvailableSpecies(3); */
         }
     };
 
     getOwned();
-    /*
-    const getLokas = async () => {
-        console.log("geting items " + items.length);
-        //if (dataFetched) return;
-        var listLokas = await contract.lokasOfOwner(account);
-        //console.log("lokas " + listLokas);
-        setDataFethced(true);
-        if (items.length == listLokas.length && items.length > 0) return;
-        for (const [index, value] of listLokas.entries()) {
-            var card_: ICard = { tokenId: 0, yield: 0 };
-            card_.tokenId = value[0];
-            card_.yield = value[1];
-            items.push(card_);
-        }
-        var cards = [];
-        var i = 0;
-        for (const [index, value] of listLokas.entries()) {
-            cards[i] = <NFTCard key={1} tokenId={value[0]} yield={value[1]} />;
-            i++;
-        }
-        console.log("items " + items[0].tokenId);
+    getClaimableDividend();
 
-        return cards;
-        //setLokaCards(items);
-    };
-    */
-    //console.log("current item " + items.length);
-    //getLokas();
-    //const cards=[];
-    /* if (!dataFetched) {
-        var cards = (async () => {
-            //const cards = await getLokas();
-            return cards;
-        })();
-
-        //getLokas();
-    } else {
-        if (!cardLoaded) {
-            //console.log("card content " + cards)
-        }
-    } */
-
-    /*const renderCards = () => {
-        //console.log("trial " + cards);
-        //items.push({ tokenId: 1, yield: 1 });
-        //while (items.length <= 0) getLokas();
-
-        //setDataFethced(true);
-        //if (items.length == listLokas.length && items.length > 0) return;
-        var i = 0;
-        /*
-        for (const [index, value] of listLokas.entries()) {
-            cards[i] = <NFTCard key={1} tokenId={value[0]} yield={value[1]} />;
-            i++;
-        }
-        var yessir = [<NFTCard key={1} tokenId={5} yield={77} />, <NFTCard key={1} tokenId={5} yield={88} />];
-        //return cards;
-        var cards = (async () => {
-            //const cards = await getLokas();
-            return cards;
-        })();
-        return items.map((card) => {
-            return <NFTCard key={card.tokenId} tokenId={card.tokenId} yield={card.yield} />;
-        });
-    }; */
-    //setAllCards(renderCards());
-    //renderCards();
-
-    //getLokas();
     useEffect(() => {
         //getLokas();
         //console.log("getting lokas use effect " + items.length);
@@ -202,9 +165,16 @@ const Hero: FunctionComponent<HeroProps> = (props) => {
                     <div>
                         <div className="px-4 py-6 text-center sm:basis-1/4 sm:pl-8">
                             <p className="mb-6 text-sm leading-6 text-gray-light-10 dark:text-gray-dark-10">
-                                <div>You have</div> <span className="items-center justify-center text-4xl text-gray-light-12 dark:text-gray-dark-12">0 USDC</span> <div>of dividend</div>
+                                <div>You have</div> <span className="items-center justify-center text-4xl text-gray-light-12 dark:text-gray-dark-12">{claimable.toFixed(2).toString()} USDC</span> <div>of dividend</div>
                                 <Link href="#">
-                                    <a className="button gradient inline-block rounded-full bg-[length:300%_300%] bg-center py-3 px-8 font-inter text-sm font-bold leading-none tracking-tight text-gray-50 hover:bg-left  hover:shadow-xl hover:shadow-blue-400/20 active:scale-95 dark:text-gray-900 sm:text-base md:text-base">Claim</a>
+                                    <a
+                                        onClick={() => {
+                                            claim();
+                                        }}
+                                        className="button gradient inline-block rounded-full bg-[length:300%_300%] bg-center py-3 px-8 font-inter text-sm font-bold leading-none tracking-tight text-gray-50 hover:bg-left  hover:shadow-xl hover:shadow-blue-400/20 active:scale-95 dark:text-gray-900 sm:text-base md:text-base"
+                                    >
+                                        Claim
+                                    </a>
                                 </Link>
                             </p>
                             <p className="text-sm font-bold leading-6 text-gray-light-12 dark:text-gray-dark-12"></p>
@@ -214,7 +184,7 @@ const Hero: FunctionComponent<HeroProps> = (props) => {
                                 <div className="relative aspect-square w-full " style={{ justifyContent: "center", alignItems: "center" }}>
                                     <div className="h-[60px] p-2 text-sm text-gray-light-12 dark:text-gray-dark-12">Total Claimed</div>
                                     <div className="flex items-center justify-center text-3xl text-slate-50">
-                                        <span className="items-center justify-center text-gray-light-12 dark:text-gray-dark-12">USDC 0</span>
+                                        <span className="items-center justify-center text-gray-light-12 dark:text-gray-dark-12">USDC {totalClaimed.toFixed(2).toString()}</span>
                                     </div>
                                 </div>
                             </div>
@@ -272,10 +242,10 @@ const Hero: FunctionComponent<HeroProps> = (props) => {
                         {" "}
                         <hr />
                     </div>
-                    <h2 className="med-hero-text">
+                    {/* <h2 className="med-hero-text">
                         Stonker <span className="gradient move-gradient bg-[length:250%_250%] bg-clip-text text-transparent transition-none sm:py-20">Treasury Farming</span>
                     </h2>
-                    <h2 className="small-hero-text">(coming soon)</h2>
+                    <h2 className="small-hero-text">(coming soon)</h2>*/}
                     <div className="md:flex lg:flex ">
                         {/*<NFTCard tokenId={0} yield={20} />
                         <NFTCard tokenId={0} yield={20} />
